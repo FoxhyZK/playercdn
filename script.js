bc6ae9e739fdea31c485aab36ca58a3b1559e340
FoxhyZK <136761132+FoxhyZK@users.noreply.github.com>
2023-08-26 17:27:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ var jwDefaults = {
     "stagevideo": false,
     "stretching": "uniform",
     "width": "100%",
-    "height": "93vh",
+    "height": "100vh",
     cast: {"appid": "00000000"},
     "base": ".",
     androidhls: true
